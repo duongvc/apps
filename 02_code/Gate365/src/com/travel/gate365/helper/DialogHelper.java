ccf4ba8da1f63e51349effc463dfe08812edd5f4
Chuong Vo <chuongvo@jmango.net>
2014-12-14 17:15:01 +1100

    refactor the DialogHelper class

--- 02_code/Gate365/src/com/travel/gate365/helper/DialogHelper.java
+++ 02_code/Gate365/src/com/travel/gate365/helper/DialogHelper.java
@@ -1,218 +1,59 @@
 package com.travel.gate365.helper;
 
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.content.DialogInterface.OnClickListener;
 
 public final class DialogHelper {
 
 	private static AlertDialog sAlert = null;
 
-	/**
-	 * Popups a alert with a given message.
-	 * @param pContext
-	 * @param pMessageResId
-	 */
-	public static void alert(Context pContext, int pTitleResId, int pMessageResId, DialogInterface.OnClickListener pHandler){
-		sAlert = new AlertDialog(pContext){
-			
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		DialogInterface.OnClickListener handler = pHandler;
-		if(handler == null){
-			handler = new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.dismiss();
-					sAlert = null;
-				}
-			};
-		}
-		sAlert.setTitle(pTitleResId);
-		sAlert.setMessage(pContext.getString(pMessageResId));
-		sAlert.setButton(pContext.getString(android.R.string.ok), handler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();
-	}
-	
-	/**
-	 * Popups a alert with a given message.
-	 * @param pContext
-	 * @param pMessageResId
-	 */
-	public static void alert(Context pContext, int pTitleResId, int pMessageResId, int iconResId, DialogInterface.OnClickListener pHandler){
-		sAlert = new AlertDialog(pContext){
-			
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		DialogInterface.OnClickListener handler = pHandler;
-		if(handler == null){
-			handler = new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.dismiss();
-					sAlert = null;
-				}
-			};
-		}
-		sAlert.setIcon(iconResId);
-		sAlert.setTitle(pTitleResId);
-		sAlert.setMessage(pContext.getString(pMessageResId));
-		sAlert.setButton(pContext.getString(android.R.string.ok), handler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();
-	}
-	
-	/**
-	 * Popups a alert with a given message.
-	 * @param pContext
-	 * @param pMessage
-	 */
-	public static void alert(Context pContext, String pTitle, String pMessage, int iconResId, DialogInterface.OnClickListener pHandler){
-		sAlert = new AlertDialog(pContext){
-			
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		DialogInterface.OnClickListener handler = pHandler;
-		if(handler == null){
-			handler = new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.dismiss();
-					sAlert = null;
-				}
-			};
-		}
-		sAlert.setIcon(iconResId);
-		sAlert.setTitle(pTitle);
-		sAlert.setMessage(pMessage);
-		sAlert.setButton(pContext.getString(android.R.string.ok), handler);
+	public static void alert(Context pContext, int pTitleResId, int pMessageResId, OnClickListener pHandler) {
+		sAlert = buildAlert(pContext, pTitleResId, pMessageResId, pHandler).create();
 		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 		sAlert.show();
 	}
 
-	/**
-	 * Popups a alert with a given message.
-	 * @param pContext
-	 * @param pMessage
-	 */
 	public static void alert(Context pContext, String pTitle, String pMessage) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(pContext);
-		builder.setTitle(pTitle).setMessage(pMessage).setCancelable(true)
-				.setNegativeButton(pContext.getString(android.R.string.ok), new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.dismiss();
-						sAlert = null;
-					}
-				});
+		sAlert = buildAlert(pContext, pTitle, pMessage, null).create();
+		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+		sAlert.show();
+	}
+
+	private static AlertDialog.Builder buildAlert(Context pContext, String pTitle, String pMessage, OnClickListener pHandler) {
+		Builder builder = new Builder(pContext).setTitle(pTitle).setMessage(pMessage).setCancelable(true);
+
+		OnClickListener handler = pHandler;
+		if (handler == null) {
+			handler = new OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.dismiss();
+					sAlert = null;
+				}
+			};
+		}
+
+		builder.setNegativeButton(android.R.string.ok, handler);
+		return builder;
+	}
+
+	private static Builder buildAlert(Context pContext, int pTitleResId, int pMessageResId, OnClickListener pHandler) {
+		return buildAlert(pContext, pContext.getString(pTitleResId), pContext.getString(pMessageResId), pHandler);
+	}
+
+	public static AlertDialog yesNoAlert(Context pContext, String pTitle, String pMessage, String pPositivebutton, String pNegativeButton,
+			OnClickListener pPositiveHandler, OnClickListener pNegativeHandler) {
+
+		Builder builder = new Builder(pContext).setTitle(pTitle).setMessage(pMessage).setCancelable(true);
+
+		builder.setNegativeButton(pNegativeButton, pNegativeHandler);
+		builder.setPositiveButton(pPositivebutton, pPositiveHandler);
+
 		sAlert = builder.create();
 		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 		sAlert.show();
-	}
-
-	/**
-	 * 
-	 * @param pContext
-	 * @param pTitle
-	 * @param pView
-	 * @param iconResId
-	 * @param pHandler
-	 */
-	public static AlertDialog alert(Context pContext, String pTitle, View pView, int iconResId, DialogInterface.OnClickListener pHandler){
-		sAlert = new AlertDialog(pContext){
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		DialogInterface.OnClickListener handler = pHandler;
-		if(handler == null){
-			handler = new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.dismiss();
-					sAlert = null;
-				}
-			};
-		}
-		sAlert.setIcon(iconResId);
-		sAlert.setTitle(pTitle);
-		sAlert.setView(pView);
-		sAlert.setButton(pContext.getString(android.R.string.ok), handler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();	
 		return sAlert;
 	}
-	
-	public static AlertDialog yesNoAlert(Context pContext, String pTitle, View pView, int pIconResId, String pPositiveButton, 
-			String pNegativeButton, DialogInterface.OnClickListener pPositiveHandler, DialogInterface.OnClickListener pNegativeHandler) {
-		sAlert = new AlertDialog(pContext){
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		
-		sAlert.setIcon(pIconResId);
-		sAlert.setTitle(pTitle);
-		sAlert.setView(pView);
-		sAlert.setButton(pPositiveButton, pPositiveHandler);
-		sAlert.setButton(pNegativeButton, pNegativeHandler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();	
-		return sAlert;
-	}
-
-	public static AlertDialog yesNoAlert(Context pContext, String pTitle, String pMessage, int pIconResId, String pPositivebutton, 
-			String pNegativeButton, DialogInterface.OnClickListener pPositiveHandler, DialogInterface.OnClickListener pNegativeHandler) {
-		sAlert = new AlertDialog(pContext){
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		
-		sAlert.setIcon(pIconResId);
-		sAlert.setTitle(pTitle);
-		sAlert.setMessage(pMessage);
-		sAlert.setButton(pPositivebutton, pPositiveHandler);
-		sAlert.setButton2(pNegativeButton, pNegativeHandler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();	
-		return sAlert;
-	}
-
-	public static AlertDialog yesNoAlert(Context pContext, String pTitle, String pMessage, String pPositivebutton, 
-			String pNegativeButton, DialogInterface.OnClickListener pPositiveHandler, DialogInterface.OnClickListener pNegativeHandler) {
-		sAlert = new AlertDialog(pContext){
-		    protected void onCreate(Bundle savedInstanceState){
-		        super.onCreate(savedInstanceState);
-		    }			
-		};
-		
-		sAlert.setIcon(0);
-		sAlert.setTitle(pTitle);
-		sAlert.setMessage(pMessage);
-		sAlert.setButton(pPositivebutton, pPositiveHandler);
-		sAlert.setButton2(pNegativeButton, pNegativeHandler);
-		sAlert.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-		sAlert.show();	
-		return sAlert;
-	}
-	
-	public static void hide(){
-		if(sAlert != null){
-			try{
-				if(sAlert.isShowing()){
-					sAlert.cancel();
-				}
-			}catch(Exception e){
-				Log.e("hide", e.getMessage(), e);
-			}
-			sAlert = null;
-		}
-	}
-		
 }
